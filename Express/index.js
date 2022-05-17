26a4b0db80a8adbf553288c630c02e8708e8c49b
ManvelmManucharyan <manvelmanucharyan98@gmail.com>
2022-05-17 23:50:05 +0400

    post

--- Express/index.js
+++ Express/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const fs = require('fs');
 const app = express();
 
-app.use((req, res, next)=>{
-    next();
+app.use(bodyParser.json());
+
+app.post('/create', (req, res)=>{
+    fs.writeFileSync('./users.json', JSON.stringify(req.body));
+    res.status(201).send('Created');
 });
 
-app.get('/',(req, res)=>{
-    res.send('Hello World');
+app.get('/users', (req,res)=>{
+    const data = fs.readFileSync('./users.json');
+    res.status(200).send(data);
 });
-
-app.get('/contact', (req,res)=>{
-    res.send("Contact");
-});
-
 
 app.listen(3000);
